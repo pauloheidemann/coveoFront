b8cc5a9d21f841bee6377e07097b5fba6643b2f6
pauloheidemann <paulo.heidemann@gmail.com>
2019-09-22 19:16:39 -0300

    implementing spa using vue resource and adding filter to the form

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,80 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="side-menu">
+      <form @submit.prevent="search()">
+        <div class="controle">
+          <label for="searchField">Generic search</label>
+          <input id="searchField" v-model="filter">
+        </div>
+        <div class="controle">
+          <label for="searchTitle">Title</label>
+          <input id="searchTitle" v-model="filterTitle">
+        </div>
+        <div class="controle">
+          <input type="checkbox" name="whiteWine"  value="Vin blanc" v-model="whiteWine"/>White Wine
+          <br>
+          <input type="checkbox" name="redWine" value="Vin rouge" v-model="redWine"/>Red Wine
+        </div>
+        <button type="submit" >Search</button>
+      </form>
     </div>
-    <router-view />
+    <div id="wines-list">
+      <li class="wines" v-for="wine of wines">
+        <meu-painel :title="wine.title" :uri="wine.uri" :thumbnail="wine.raw.tpthumbnailuri" 
+          :category="wine.raw.tpcategorie"/>
+      </li>
+    </div>
   </div>
 </template>
+
+<script>
+import Painel from '../src/components/Painel.vue';
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
+
+export default {
+  components: {
+    'meu-painel' : Painel,
+    VueSlider
+  },
+  data() {
+
+    return {
+      wines: [], 
+      filter: '',
+      filterTitle: '',
+      sliderValue: '',
+      whiteWine: '',
+      redWine: ''
+    }
+  },
+  created() {
+    this.$http.post('http://localhost:8080/v1/coveo', " ")
+      .then(res => res.json())
+      .then(wines => this.wines = wines, err => console.log('erro ' + err));
+  },
+  methods: {
+    search() {
+      let mountedFilter = ' ';
+      if(this.filter != '')
+        mountedFilter = mountedFilter + this.filter;
+      if(this.filterTitle != '')
+        mountedFilter = mountedFilter + " @title=" + this.filterTitle;
+      if(this.whiteWine == true && this.redWine == true) {
+        mountedFilter = mountedFilter + " @tpcategorie=Vin rouge OR @tpcategorie=Vin blanc "
+      } else if(this.whiteWine == true) {
+        mountedFilter = mountedFilter + " @tpcategorie=Vin blanc "
+      } else if(this.redWine == true) {
+        mountedFilter = mountedFilter + " @tpcategorie=Vin rouge "
+      }
+      console.log(mountedFilter);     
+      this.$http.post('http://localhost:8080/v1/coveo', mountedFilter)
+      .then(res => res.json())
+      .then(wines => this.wines = wines, err => console.log('erro ' + err));
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,17 +83,25 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+  width: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#side-menu {
+  float: left;
+  width: 30%;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#wines-list {
+  width: 70%;
+  float: right;
+}
+
+.wines {
+  list-style-type: none;
+}
+
+.controle {
+  text-align: left;
+  margin-bottom: 10px;
 }
 </style>
---
+++ src/components/Painel.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="painel">
+        <a :href="uri" target="_blank">
+            <h2 class="painel-title">{{ title }}</h2>
+        </a>
+        <div class="painel-content" >
+            <slot></slot>
+            <img :src="thumbnail">
+            <br>
+            <span>{{ category }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'uri', 'thumbnail', 'category']
+}
+
+</script>
+
+<style scoped>
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-title {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+
+  * {
+      box-shadow: 5px 5px 5px;
+  }
+
+</style>
